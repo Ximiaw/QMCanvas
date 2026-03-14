39be48b65febf22a0e2654ebba224cbf01271245
Ximi <m3311764705@outlook.com>
2026-03-14 16:57:32 +0800

    feat(QMCanvasScene): 添加Location类的构造函数及视口相关方法
    增加了Location类的构造函数，用于初始化基础矩形区域。添加了获取和设置基础矩形区域、视口矩形区域的方法。实现了视口矩形区域的缩放和外扩功能，考虑了鼠标位置的影响，并确保视口区域在基础矩形区域内。添加了获取和设置缩放倍率、外扩倍率、最大和最小缩放倍率的方法。
    
    Location::Location(QRectF baseRect)：构造函数，初始化基础矩形区域。
    setBaseRect(QRectF baseRect)：设置基础矩形区域。
    baseRect()：获取基础矩形区域。
    setViewportRect(QRectF rect)：设置视口矩形区域。
    viewportRect()：获取视口矩形区域。
    viewportRectRM()：计算并返回缩放外扩后的视口矩形区域。
    setMousePoint(QPoint point)：设置鼠标位置。
    setFactor(qreal factor)：设置每次缩放的倍率。
    factor()：获取每次缩放的倍率。
    setRatio(qreal ratio)：设置当前图像的倍率。
    ratio()：获取当前图像的倍率。
    setExtraViewportMargin(qreal rate)：设置外扩倍率。
    extraViewportMargin()：获取外扩倍率。
    setMaxRatio(qreal max)：设置最大缩放倍率。
    maxRatio()：获取最大缩放倍率。
    setMinRatio(qreal min)：设置最小缩放倍率。
    minRatio()：获取最小缩放倍率。

--- src/QMCanvasScene/Location.h
+++ src/QMCanvasScene/Location.h
@@ -4,13 +4,51 @@
 #ifndef MAIN_TRANSFORM_H
 #define MAIN_TRANSFORM_H
 
-#include <QRect>
+#include <QRectF>
 #include <QPoint>
 
 class Location
 {
+private:
+    QRectF baseRect_;//基础区域，所有位置、空间都基于这个，相当于未缩放的图片大小
+    QRectF viewportRect_;//未缩放未外扩的viewport区域
+    //QRectF viewportRectRM_;//缩放外扩后的viewport区域
 
+    bool considerMouse_;//为true时根据鼠标计算偏移
+    QPoint mousePoint_;//鼠标指向的位置，是未缩放未外扩的
 
+    qreal factor_=1.0;//每次缩放倍率，和viewportRate相乘
+    qreal ratio_=1.0;//当前图像倍率，如1.2为原图的1.2倍
+    qreal marginRate_=1.0;//当前外扩倍率，1为默认值
+    qreal maxRatio_=20.0;//最大缩放倍率
+    qreal minRatio_=0.01;//最小缩放倍率
+public:
+    Location(QRectF baseRect);
+    ~Location()=default;
+
+    QRectF baseRect();
+    void setBaseRect(QRectF baseRect);
+
+    QRectF viewportRect();
+    void setViewportRect(QRectF rect);
+
+    QRectF viewportRectRM();
+    void setMousePoint(QPoint point);
+
+    qreal maxRatio() const;
+    void setMaxRatio(qreal max);
+
+    qreal minRatio() const;
+    void setMinRatio(qreal min);
+
+    qreal factor() const;
+    void setFactor(qreal factor);
+
+    qreal extraViewportMargin() const;
+    void setExtraViewportMargin(qreal rate);
+
+    qreal ratio() const;
+    void setRatio(qreal ratio);
 };
 
 
